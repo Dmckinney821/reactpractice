7f42dee3f4e2e5107ce95642c102048c3c787885
Dan McKinney <3dhdaniel@gmail.com>
2018-07-18 11:32:11 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
 import React from 'react';
 import Counter from './Counter';
-const handleClick = () => {
-  console.log('hay');
-};
+// const handleClick = () => {
+//   console.log('hay');
+// };
 
-function convertNumToCounter(value) {
+function convertNumToCounter(obj) {
   return(
-    <Counter initialValue={value} />
+    <Counter key = {obj.id} initialValue={obj.value} />
   );
 }
 
 // This is not quite the React way but just an example
-let counterValues = [42, 33, 65, 4, 1];
-let counterElements = counterValues.map(convertNumToCounter)
-
-// let counterElements = counterValues.map(value => <Counter initialValue={value} />);
-const App = () => {
-//  return <h1>Hello</h1>;
-    return (
-    <div className='container'>
-      <button onClick={handleClick}>+</button>
+// let counterValues = [42, 33, 65, 4, 1];
+// let counterElements = counterValues.map(convertNumToCounter)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counterValues: [{id: 54321, value: 33}]
+    };
+  }
+  _handleClick = () => {
+   let newObj = {
+     id: (new Date()).getTime(),
+     value: 0
+   };
+    this.setState({
+      counterValues: this.state.counterValues.concat(newObj)
+    })
+  }
+    render() {
+      return (
+      <div className='container'>
+        <button onClick={this._handleClick}>+</button>
       <div className='counterBox'>
-        {/* /* <div className='counter'>3</div>
-        <div className='counter'>1</div>
-        <div className='counter'>1</div> */ }
-          {counterElements}
-
-      </div>
+          {this.state.counterValues.map(convertNumToCounter)}
+           </div>
     </div>
     );
+    }
+  }
 
-};
+// // let counterElements = counterValues.map(value => <Counter initialValue={value} />);
+// const App = () => {
+// //  return <h1>Hello</h1>;
+//     return (
+//     <div className='container'>
+//       <button onClick={handleClick}>+</button>
+//       <div className='counterBox'>
+//           {counterElements}
+//            </div>
+//     </div>
+//     );
+
+// };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -7,16 +7,25 @@ class Counter extends React.Component {
         this.state ={
             currentValue: props.initialValue
         };
+        setInterval(this._increaseValue, 1000);
     }
     render() {
         return (
             <div className='counter'>
             {this.state.currentValue}
             </div>
-        )
+        );
+    }
+    _increaseValue = () => {
+        // calculate the new currentValue
+        let newCurrentValue = this.state.currentValue + 1;
+        // then set the new currentValue in state
+        // but, I must call this.setState as I can't alter this.state directly
+        this.setState({
+            currentValue: newCurrentValue
+        });
     }
 }
-
 
 
 
